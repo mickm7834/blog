b359ce5ca12a1e9499fc674df01bbbbafd346f54
mick <mickm7834@gmail.com>
2021-12-12 16:05:07 +0100

    Creation entity InfosGenerales.

---
+++ blog/src/main/java/com/doranco/blog/entity/InfosGenerales.java
@@ -0,0 +1,27 @@
+package com.doranco.blog.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InfosGenerales implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Date dateCreation;
+	private Date dateModification;
+}
