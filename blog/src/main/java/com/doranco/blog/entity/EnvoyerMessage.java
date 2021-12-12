5f0733575111f46e72c004258a432a7dbef56d89
mick <mickm7834@gmail.com>
2021-12-12 17:36:01 +0100

    Creation entity EnvoyerMessage.

---
+++ blog/src/main/java/com/doranco/blog/entity/EnvoyerMessage.java
@@ -0,0 +1,28 @@
+package com.doranco.blog.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnvoyerMessage implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idMessage;
+	private String contenu;
+	private Date dateEnvoi;
+	private boolean lu;
+}
