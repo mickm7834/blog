de321c7f8aca34502d65bd6c23d33fb3bbf49ec1
mick <mickm7834@gmail.com>
2021-12-12 17:27:05 +0100

    Creation entity EnvoyerDemande.

---
+++ blog/src/main/java/com/doranco/blog/entity/EnvoyerDemande.java
@@ -0,0 +1,32 @@
+package com.doranco.blog.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnvoyerDemande implements Serializable {
+	
+	public enum Etat{
+		ATTENTE, ACCEPTER, REFUSER
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Etat etat;
+	private Date dateEnvoi;
+}
