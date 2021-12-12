bcbc48aeda39b8f5724ad271d647ef93a1e9dd06
mick <mickm7834@gmail.com>
2021-12-12 17:17:39 +0100

    Creation entity PublicationMedia.

---
+++ blog/src/main/java/com/doranco/blog/entity/PublicationMedia.java
@@ -0,0 +1,19 @@
+package com.doranco.blog.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublicationMedia extends Publication implements Serializable {
+	private String referenceMedia;
+}
